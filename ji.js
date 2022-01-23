299b4c119e7871a31d7447e9d7f1bd40516bdaec
Mudassar Iqbal <Muhammadmudasar438@gmail.com>
2022-01-23 22:18:11 +0500

    beat the button

---
+++ ji.js
@@ -0,0 +1,59 @@
+b0.onclick = function(){
+    document.getElementById('b0').classList.add('active');
+    var mudasar = document.getElementById('Alrem');
+    mudasar.play();
+
+}
+
+b1.onclick = function(){
+    document.getElementById('b1').classList.add('active');
+    var mudasar = document.getElementById('dog');
+    mudasar.play();
+}
+
+
+b2.onclick = function(){
+    document.getElementById('b2').classList.add('active');
+    var mudasar = document.getElementById('bird');
+    mudasar.play();
+}
+
+b3.onclick = function(){
+    document.getElementById('b3').classList.add('active');
+    var mudasar = document.getElementById('game');
+    mudasar.play();
+}
+
+
+b4.onclick = function(){
+    document.getElementById('b4').classList.add('active');
+    var mudasar = document.getElementById('forest');
+    mudasar.play();
+}
+
+b5.onclick = function(){
+    document.getElementById('b5').classList.add('active');
+    var mudasar = document.getElementById('lion');
+    mudasar.play();
+}
+
+b6.onclick = function(){
+    document.getElementById('b6').classList.add('active');
+    var mudasar = document.getElementById('impect');
+    mudasar.play();
+}
+
+b7.onclick = function(){
+    document.getElementById('b7').classList.add('active');
+    var mudasar = document.getElementById('dream');
+    mudasar.play();
+}
+
+b8.onclick = function(){
+    document.getElementById('b8').classList.add('active');
+    var mudasar = document.getElementById('fire');
+    mudasar.play();
+}
+
+
+
